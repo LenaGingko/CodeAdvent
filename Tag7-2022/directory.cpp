00e55799cb52e7f9a886813b47c6c56933947dc3
lbeckman <lena_henrike.beckmann@stud.htwk-leipzig.de>
2023-04-01 21:11:39 +0200

    Tag 7 angefangen

--- Tag7-2022/directory.cpp
+++ Tag7-2022/directory.cpp
@@ -5,16 +5,22 @@
 
 int main()
 {
+    std::vector<std::string> zeile;
+
     std::ifstream fileread ( "directory.txt" );
     if(!fileread){std::cerr<<"\nFehler. Datei wird nicht gelesen.\n";}
 
+
     if(fileread.is_open()){
+        std::string line;
 
-        while(!fileread.eof()){ 
-
+        while(getline(fileread, line)){
+            zeile.push_back(line);
         }
         
     }
 
-
+    for(int i = 0; i < zeile.size(); i++){     
+        std::cout<<zeile[i]<<"\n";
+    }
 }
